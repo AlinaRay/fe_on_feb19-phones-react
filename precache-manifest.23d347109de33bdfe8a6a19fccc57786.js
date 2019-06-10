9e7d55d29c60fb68d23793969a27847f86d1c16c
Alina Ray <a.alina.ray@gmail.com>
2019-06-10 17:45:49 +0300

    Updates

--- precache-manifest.23d347109de33bdfe8a6a19fccc57786.js
+++ precache-manifest.23d347109de33bdfe8a6a19fccc57786.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bccfd664c946a9206a44530c0d6c5f3",
+    "revision": "bab9f178d58d8f89f17fee923574cd6f",
     "url": "/fe_on_feb19-phones-react/index.html"
   },
   {
-    "revision": "a7c066b342f2c74f30bd",
+    "revision": "1e79ac0896dfcb3e63eb",
     "url": "/fe_on_feb19-phones-react/static/css/main.74074b87.chunk.css"
   },
   {
-    "revision": "681b51057d74cfa8975d",
-    "url": "/fe_on_feb19-phones-react/static/js/2.65aa1cca.chunk.js"
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/fe_on_feb19-phones-react/static/js/2.a5adb38c.chunk.js"
   },
   {
-    "revision": "a7c066b342f2c74f30bd",
-    "url": "/fe_on_feb19-phones-react/static/js/main.d9da4509.chunk.js"
+    "revision": "1e79ac0896dfcb3e63eb",
+    "url": "/fe_on_feb19-phones-react/static/js/main.184a1aba.chunk.js"
   },
   {
     "revision": "13ef023f66a5d3f30d63",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fe_on_feb19-phones-react/precache-manifest.4321a3f769abe4bdae7dfdebc8fba4d9.js"
+  "/fe_on_feb19-phones-react/precache-manifest.23d347109de33bdfe8a6a19fccc57786.js"
 );
 
 self.addEventListener('message', (event) => {
